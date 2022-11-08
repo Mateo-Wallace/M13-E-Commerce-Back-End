60aca335a6c61fb2f666cbe7ccf4578876abb11a
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-08 10:05:22 -0500

    update functioning

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -37,10 +37,11 @@ router.get('/:id', async(req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+			"category_id": 1,
+			"tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -100,15 +101,31 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) => res.json(`Product ${req.params.id} has been updated`))
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(`Product ${req.params.id} has been deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
