020a1fb1a6051a2267de24e97c35493e4cc92124
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-08 08:12:47 -0500

    category delete request functioning. Category routes COMPLETE

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,8 +62,24 @@ router.put('/:id', async(req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(`Category ${req.params.id} has been deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
