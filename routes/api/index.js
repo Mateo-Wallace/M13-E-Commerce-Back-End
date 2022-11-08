b059e7c0ef13560da8e0df44e4da3bce1eff3607
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-08 06:31:36 -0500

    added error json messages based on route location to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,4 +7,12 @@ router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+router.use((req, res) => {
+    res.status(400).json({
+      "Message": "Almost there! For available routes please check the README at the link below",
+      "Link": "https://github.com/Mateo-Wallace/M13-E-Commerce-Back-End",
+      "Author": "Mateo Wallace"
+    });
+  });
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route! \n Check the README for potential routes.</h1>")
+  res.status(400).json({
+    "Message": "No information on this route. For available routes please check the README at the link below",
+    "Link": "https://github.com/Mateo-Wallace/M13-E-Commerce-Back-End",
+    "Author": "Mateo Wallace"
+  });
 });
 
 module.exports = router;
