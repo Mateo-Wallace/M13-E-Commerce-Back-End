2cf8d30b4f4c2c277e54cb59e434a9f63d135b83
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-08 07:22:03 -0500

    category update completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,23 @@ router.post('/', async(req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.update(
+      req.body,
+      {where: {id: req.params.id}}
+      );
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(`Category ${req.params.id} has been updated`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
